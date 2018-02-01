ceaeafbabf1c39a8c3f7a7f792494bdb6b1d2bb9
hansonr <hansonr@stolaf.edu>
2018-02-01 09:00:53 -0600

    JSmolAppletObject

--- src/org/jmol/util/JmolAudio.java
+++ src/org/jmol/util/JmolAudio.java
@@ -79,8 +79,6 @@ public class JmolAudio implements LineListener, JmolAudioPlayer {
       /**
        * @j2sNative
        * 
-       * jmol = Jmol;
-       * 
        */
       {
         getClip();
