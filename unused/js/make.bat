bf4821e17679cb2be479f3318127c71c468fe5b5
hansonr <hansonr@stolaf.edu>
2023-02-11 02:44:26 -0600

    removing unused/js

--- unused/js/make.bat
+++
@@ -1,6 +0,0 @@
-copy /b jquery.js+j2sJQueryExt.js+j2sApplet.js+j2sSwingJS.js+JSmolCore.js+JSmolApplet.js+JSmolApi.js+JSmolControls.js+JSmol.js+JSmolConsole.js+JSmolMenu.js ..\JSmolSwingJS.js
-copy ..\JSmolSwingJS.js c:\temp
-dir ..\JSmolSwingJS.js
-
-
-rem not adding SJSApplet.js to this
